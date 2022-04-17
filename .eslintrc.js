3e6ea02368caf16a5bc750fabcd397e6355bc0af
junkor-1011 <tomoyukizxy@gmail.com>
2022-04-17 22:21:04 +0900

    chore(package): install storybook with webpack5

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,22 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: ['airbnb', 'next/core-web-vitals', 'prettier'],
+  extends: ['airbnb', 'next/core-web-vitals', 'plugin:storybook/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 'latest',
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', 'tsx'] }],
-  },
+    'react/jsx-filename-extension': [1, {
+      extensions: ['.js', '.jsx', '.ts', 'tsx']
+    }]
+  }
 };
