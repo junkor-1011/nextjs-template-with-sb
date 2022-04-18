68f5383914afed01c423add01a48810c07480b35
junkor-1011 <tomoyukizxy@gmail.com>
2022-04-19 00:09:07 +0900

    chore(linter): arrange eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,50 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: ['airbnb', 'next/core-web-vitals', 'plugin:storybook/recommended', 'prettier'],
+  extends: [
+    'airbnb',
+    'next',
+    'plugin:@next/next/recommended',
+    'next/core-web-vitals',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [1, {
-      extensions: ['.js', '.jsx', '.ts', 'tsx']
-    }]
-  }
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', '.ts', 'tsx'],
+      },
+    ],
+    'react/require-default-props': 'off',
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.stories.tsx'],
+      },
+    ],
+    'import/extensions': [
+      2,
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        jsn: 'always',
+      },
+    ],
+  },
 };
