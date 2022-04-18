fc49524371e0e438241b1098050b24626569d45b
junkor-1011 <tomoyukizxy@gmail.com>
2022-04-19 02:23:33 +0900

    chore: enable scss in storybook thanks to https://dev.classmethod.jp/articles/tried-to-add-storybook-to-nextjs-project/

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,8 +10,25 @@ module.exports = {
   ],
   webpackFinal(config) {
     config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')];
-
     config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
+
+    // Sass & CSS Modules
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: {
+              auto: true,
+            },
+          },
+        },
+        "sass-loader",
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
 
     return config;
   },
