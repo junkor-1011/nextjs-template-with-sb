bcb087e7684812e9fb2375699f58566b8e7d7059
junkor-1011 <tomoyukizxy@gmail.com>
2022-04-19 02:31:46 +0900

    chore(add-example): tutorial from get-started-with-storybook-and-next-js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
+import * as NextImage from 'next/image'
+
+import '../src/styles/globals.css'
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
