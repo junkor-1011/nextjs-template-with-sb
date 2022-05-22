a0cd97accf1cd1b0ed18df3a3301bc730d5b23b7
junkor-1011 <tomoyukizxy@gmail.com>
2022-05-23 00:27:21 +0900

    chore: tsconfig

--- src/stories/pages/home.stories.tsx
+++ src/stories/pages/home.stories.tsx
@@ -1,9 +1,8 @@
-// import Home from "../../pages/index";
-import Home from "pages/index";
+import Home from '@/pages';
 
 export default {
-  title: "Pages/Home",
+  title: 'Pages/Home',
   component: Home,
 };
 
-export const HomePage = () => <Home />
+export const HomePage = () => <Home />;
